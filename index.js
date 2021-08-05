8f610b9305516350fb07444a5b10ef6b2d86a8f1
Kunal Pareek <kunalp@outlook.in>
2021-08-06 00:06:23 +0530

    Added static file support

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.use('/assets', express.static(__dirname + '/assets'));
 
 const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
